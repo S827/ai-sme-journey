fe1b258db4f1bd41f69b59c6b4c3fc8ed51d0e7c
s827 <dskumar827@gmail.com>
2025-08-11 02:56:57 +0530

    feat(python-basics): add variables, types and operators example

---
+++ 01_python_basics/day1_variables.py
@@ -0,0 +1,29 @@
+﻿name = "Divya"
+age = 28
+height = 5.9
+is_learning = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning AI SME Journey:", is_learning)
+
+# Arithmetic example
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+# Comparison Example
+print("Is a greater than b?", a > b)
+print("Is a equal to b?", a == b)
+
+# Logical Example
+print("True and False:", True and False)
+print("True or False:", True or False)
+print("Not True:", not True)
