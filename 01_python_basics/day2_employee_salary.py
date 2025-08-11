b07173cc66dd30d4ab14d694e49e6fb7444f87eb
s827 <dskumar827@gmail.com>
2025-08-12 01:47:58 +0530

    feat(python-basics): add Day2 Employee Salary Calculator

---
+++ 01_python_basics/day2_employee_salary.py
@@ -0,0 +1,13 @@
+﻿name = input()
+age = int(input())
+salary = int(input())
+bonus_percent = float(input())
+print('Employee Name:', name)
+print('Age:', age)
+print('Monthly Salary: $' + str(salary))
+annual_salary_without_bonus = salary * 12
+annual_bonus_amount = 12 * salary * bonus_percent
+annual_salary_with_bonus = annual_salary_without_bonus + annual_bonus_amount
+print('Annual Salary (without bonus):', annual_salary_without_bonus)
+print('Annual Bonus Amount:', annual_bonus_amount)
+print('Total Annual Salary (with bonus): $' + str(annual_salary_with_bonus))
