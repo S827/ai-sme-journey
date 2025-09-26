30d98997a444412e034c72716e6631712ea26a9b
s827 <dskumar827@gmail.com>
2025-09-26 22:48:01 +0530

    added coursera c2 toolsForDS M6 Lab assignment

---
+++ 01_python_basics/Modules/import.py
@@ -0,0 +1,8 @@
+# from package.math import add
+# print(add(1, 2))
+# from package import math
+# print(math.add(1, 2))
+# print(math.sub(0, 0))
+
+from package.subpackages.mul import mul
+print(mul(1, 2))
